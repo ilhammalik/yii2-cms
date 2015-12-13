43b60c2ced1f01f6ed88070ae848a9efd9091123
Kantor <ilhammalik19@gmail.com>
2015-12-13 10:36:25 +0700

    delete
    delete

--- backend/web_old/theme/js/pages/formsGeneral.js
+++
@@ -1,5 +0,0 @@
-/*
- *  Document   : formsGeneral.js
- *  Author     : pixelcave
- */
-var FormsGeneral=function(){return{init:function(){$(".toggle-bordered").click(function(){$(this).parents(".block").find("form").toggleClass("form-bordered")})}}}();
